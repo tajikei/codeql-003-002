f44fce6b5dd1d92da8060db8837f8bce0df909c5
n3rd-vignesh <65106007+n3rd-vignesh@users.noreply.github.com>
2023-07-03 12:33:41 +0530

    Create sample.java

---
+++ src/main/java/com/example/project/sample.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class CombinedProgram {
+    private static final Logger logger = LogManager.getLogger(CombinedProgram.class);
+
+    public static void main(String[] args) {
+        try {
+            // Sensitive command-line argument
+            String username = "myusername";
+            String password = "mypassword";
+
+            // Sensitive environment variable
+            String databaseUrl = "jdbc:mysql://localhost:3306/mydatabase";
+
+            // Build the command with sensitive information
+            String command = "mysql -u" + username + " -p" + password + " -h" + databaseUrl;
+
+            // Create a process and execute the command
+            Process process = Runtime.getRuntime().exec(command);
+
+            // Wait for the process to complete
+            int exitCode = process.waitFor();
+
+            if (exitCode == 0) {
+                System.out.println("Process executed successfully.");
+            } else {
+                System.out.println("Process execution failed.");
+            }
+
+            // Logging password change
+            String newPassword = "Pass@0rd";
+            logger.debug("User password changed: " + newPassword);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
